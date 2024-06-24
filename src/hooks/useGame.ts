d3e740ec2fc75d34f712222499de453f9319c9a1
Parham Abdolmohammadi <parham.abdo@gmail.com>
2024-06-24 12:24:08 -0700

    Implement infinite queries

--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import  { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatform";
@@ -15,18 +15,22 @@ export interface Game {
 }
 
 const useGames = (gameQuery: GameQuery) =>
-  useQuery<FetchResponse<Game>, Error>({
+  useInfiniteQuery<FetchResponse<Game>, Error>({
   queryKey: ['games', gameQuery],
-  queryFn: () =>
+  queryFn: ({pageParam = 1}) =>
     apiClient.getAll({ params:
       {
        genres: gameQuery.genre?.id,
         parent_platform: gameQuery.platform?.id,
         ordering: gameQuery.sortOrder,
-        search: gameQuery.searchText
+        search: gameQuery.searchText,
+        page: pageParam
        } })
   ,
   staleTime: 24 * 60 * 60 * 1000,
+  getNextPageParam: (lastPage, allPages) => {
+    return  lastPage.next ? allPages.length + 1: undefined;
+ }
 })
 
 // const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {
