c87ce50faa68955579a9e9a09a2ff2e1cd059485
Parham Abdolmohammadi <parham.abdo@gmail.com>
2024-06-23 17:30:45 -0700

    Fetching genres using React Query

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,8 @@
-import useData from "./useData";
+import useData, { FetchResponse } from "./useData";
 import genres from "../data/genres";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+
 
 export interface Genre {
 id: number;
@@ -9,6 +12,10 @@ image_background: string;
 
 
 // const useGenres = () => useData<Genre>('/genres');//first way using api changed in 8.36
-const useGenres = () => ({data: genres, isLoading: false, error: null});
-
+const useGenres = () => useQuery({
+    queryKey: ['genres'],
+    queryFn: () => apiClient.get<FetchResponse<Genre>>('./genres').then(res => res.data),//we dont need to use Genre[] as Genre will be automatically transformed to Genre[] in FetchResponse 
+    staleTime: 24 * 60 * 60 * 1000,//for 24 hours our initial data will be fresh so no request will be sent to the backend until then
+    initialData: {count: genres.length, results: genres} //instead a spinner we show them a static data
+});
 export default useGenres;
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,32 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App";
 import theme from "./theme";
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    // queries: {
+    //   retry: 3, //if a query fails react will retry that 3 more times
+    //   cacheTime: 300_000, //if a query has no observer(no component is using that) that query is considered inactive and garbage collected
+    //   staleTime: 10 * 1000, //how long a data is considered fresh this has the default value of zero which means the moment we get the data is treated as old so the next time we need the same piece of data react query will refresh fresh data from the backend
+    //   refetchOnWindowFocus: false, //the default value is true
+    //   refetchOnReconnect: false, //default value is true, if the client goes offline and come back online react query will refetch our query
+    //   refetchOnMount: true, //default value is true our queries should be refetched when a component mounts for the first time
+    // },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript
-        initialColorMode={theme.config.initialColorMode}
-      ></ColorModeScript>
-      <App />
-    </ChakraProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript
+          initialColorMode={theme.config.initialColorMode}
+        ></ColorModeScript>
+        <App />
+        <ReactQueryDevtools />
+      </ChakraProvider>
+    </React.StrictMode>
+  </QueryClientProvider>
 );
