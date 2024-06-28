eb92d66e27515add69987272c20204d2d1151338
Parham Abdolmohammadi <parham.abdo@gmail.com>
2024-06-27 22:40:12 -0700

    Fetch Game Details

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,15 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width="300" borderRadius={10} overflow="hidden">
+    <Box
+      _hover={{
+        transform: "scale(1.03)",
+        transition: "transform .15s ease-in",
+      }}
+      width="300"
+      borderRadius={10}
+      overflow="hidden"
+    >
       {children}
     </Box>
   );
--- src/hooks/useGame.ts
+++ src/hooks/useGame.ts
@@ -1,50 +1,12 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
-import ms from "ms";
-import APIClient, { FetchResponse } from "../services/api-client";
-import useGameQueryStore from "../store";
-import { Platform } from "./usePlatform";
+import { useQuery } from "@tanstack/react-query";
+import APIClient from "../services/api-client";
+import { Game } from "./useGames";
 
-const apiClient = new APIClient<Game>('/games');
-export interface Game {
-  id: number;
-  name: string;
-  background_image: string;
-  parent_platforms: {platform: Platform}[]//array of objects where each object has a property of platform of type Platform
-  metacritic: number;
-  rating_top: number;
-}
+const apiClient = new APIClient<Game>('/games')
 
+const useGame = (slug: string) => useQuery({
+queryKey: ['games', slug],
+queryFn: () => apiClient.get(slug)
+})
 
-const useGames = () => {
-  const gameQuery = useGameQueryStore((s) => s.gameQuery);
-  return useInfiniteQuery<FetchResponse<Game>, Error>({
-    queryKey: ['games', gameQuery],
-    queryFn: ({pageParam = 1}) =>
-      apiClient.getAll({ params:
-        {
-         genres: gameQuery.genreId,
-          parent_platform: gameQuery.platformId,
-          ordering: gameQuery.sortOrder,
-          search: gameQuery.searchText,
-          page: pageParam
-         } })
-    ,
-    staleTime: ms('24h'),
-    getNextPageParam: (lastPage, allPages) => {
-      return  lastPage.next ? allPages.length + 1: undefined;
-   }
-  })
-}
- 
-
-// const useGames = (gameQuery: GameQuery) => useData<Game>('/games', {
-//   params:
-//    {
-//     genres: gameQuery.genre?.id,
-//      parent_platform: gameQuery.platform?.id,
-//      ordering: gameQuery.sortOrder,
-//      search: gameQuery.searchText
-//     } }, 
-//     [gameQuery]);
-
-export default useGames;
+export default useGame;
