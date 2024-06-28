f2f29983b2615fe5819adbaaa12ace3546f22266
Parham Abdolmohammadi <parham.abdo@gmail.com>
2024-06-27 21:51:35 -0700

    Handle Errors.

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./assets/pages/Layout";
 import HomePage from "./assets/pages/HomePage";
 import GameDetailPage from "./assets/pages/GameDetailPage";
+import ErrorPage from "./assets/pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "games/:id", element: <GameDetailPage /> },
