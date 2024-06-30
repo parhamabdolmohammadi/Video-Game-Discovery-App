dc63da98aee0401f8d0e0fc7795c357618dcd4f2
Parham Abdolmohammadi <parham.abdo@gmail.com>
2024-06-29 17:17:48 -0700

    fix the navbar + Logo + layout in the game detail page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,18 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import { Link } from "react-router-dom";
 
 const NavBar = () => (
   <HStack padding="10px">
     {
       //in order to layout our items horizontally we use hstack
     }
-    <Image src={logo} boxSize="60px"></Image>
+    <Link to={"/"}>
+      {" "}
+      <Image src={logo} boxSize="60px" objectFit={"cover"}></Image>
+    </Link>
+
     <SearchInput></SearchInput>
     <ColorModeSwitch></ColorModeSwitch>
   </HStack>
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -2,18 +2,21 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import React, { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
+import { useNavigate } from "react-router-dom";
 
 const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   // const {setSearchText} = useGameQueryStore(); with this, this component will be dependent on our game query store, anytime any value in this store changes this component will re render
   //a better approach is to use a selector
   const setSearchText = useGameQueryStore((store) => store.setSearchText);
+  const navigate = useNavigate();
 
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         if (ref.current) setSearchText(ref.current.value);
+        navigate("/");
       }}
     >
       <InputGroup>
