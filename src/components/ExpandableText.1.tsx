7fd2430439da75afeec61a8a7ad3684700931f4e
ParhamAbdolmohammadi <122247091+parhamabdolmohammadi@users.noreply.github.com>
2024-07-09 12:44:29 -0700

    Update ExpandableText.1.tsx

--- src/components/ExpandableText.1.tsx
+++ src/components/ExpandableText.1.tsx
@@ -1,12 +1,1 @@
-import React, { useState } from "react";
-import { Props } from "./ExpandableText";
-import { Text } from "@chakra-ui/react";
 
-export const ExpandableText = ({ children }: Props) => {
-  const [expanded, setExpanded] = useState(false);
-  const limit = 300;
-
-  if (children.length <= limit) return <Text>{children}</Text>;
-
-  return <div>ExpandableText</div>;
-};
